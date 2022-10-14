4a096b0fb2e6e819015262fc612a241dda027d5c
Arora Nirav <aroranirav2@outlook.com>
2022-10-14 18:59:09 -0500

    added new question

---
+++ Q155MinStack/Q155MinStack/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Q155MinStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public class MinStack
+        {
+            private readonly Stack<(int, int)> _stack;
+            public MinStack()
+            {
+                _stack = new Stack<(int, int)>();
+            }
+
+            public void Push(int val)
+            {
+                _stack.Push((val, _stack.Count == 0 ? val : val < _stack.Peek().Item2 ? val : _stack.Peek().Item2));
+            }
+
+            public void Pop()
+            {
+                _stack.Pop();
+            }
+
+            public int Top()
+            {
+                return _stack.Peek().Item1;
+            }
+
+            public int GetMin()
+            {
+                return _stack.Peek().Item2;
+            }
+        }
+    }
+}
