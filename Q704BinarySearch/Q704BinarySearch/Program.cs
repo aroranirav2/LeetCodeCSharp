531d760ce2d923058d4cabee0663fc531c57062b
Arora Nirav <aroranirav2@outlook.com>
2022-10-14 19:41:43 -0500

    added binary search program

---
+++ Q704BinarySearch/Q704BinarySearch/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Q704BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(Search(new[] { -1, 0, 3, 5, 9, 12 }, 9));
+        }
+        public static int Search(int[] nums, int target)
+        {
+            int left = 0, right = nums.Length - 1;
+            while (left <= right)
+            {
+                int mid = (right + left) / 2;
+                if (nums[mid] == target)
+                    return mid;
+
+                if (nums[mid] < target)
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+            return -1;
+        }
+    }
+}
