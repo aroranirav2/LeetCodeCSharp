54f7333b6e03b3845f620b24afc80d23739e94e6
Arora Nirav <aroranirav2@outlook.com>
2022-10-17 19:32:52 -0500

    modified q206

--- Q206ReverseLinkedList/Q206ReverseLinkedList/Program.cs
+++ Q206ReverseLinkedList/Q206ReverseLinkedList/Program.cs
@@ -6,10 +6,46 @@ namespace Q206ReverseLinkedList
     {
         static void Main(string[] args)
         {
+            ListNode head = CreateList();
+            head = ReverseList(head);
+        }
+        public static ListNode CreateList()
+        {
+            Console.Write("Enter number of Nodes: ");
+            int totalNumber = Convert.ToInt32(Console.ReadLine());
+            if (totalNumber < 1)
+            {
+                Console.WriteLine("Empty list");
+                return null;
+            }
+            ListNode start = null;
+            int number;
+            for (var i = 0; i < totalNumber; i++)
+            {
+                Console.Write("Enter number: ");
+                number = Convert.ToInt32(Console.ReadLine());
+                InsertAtEnd(number, ref start);
+            }
+            return start;
+        }
+        public static void InsertAtEnd(int number, ref ListNode start)
+        {
+            ListNode temp = new ListNode(number, null);
+            if (start == null)
+            { 
+                start = temp;
+                return;
+            }
+            ListNode node = start;
+            while (node.next != null)
+            {
+                node = node.next;
+            }
+            node.next = temp;
         }
         public static ListNode ReverseList(ListNode head)
         {
-            ListNode? previous = null, next;
+            ListNode previous = null, next;
             while (head != null)
             {
                 next = head.next;
