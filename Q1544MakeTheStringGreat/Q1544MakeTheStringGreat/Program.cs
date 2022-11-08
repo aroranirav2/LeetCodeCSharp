a3598f431a8375231b3e9da8d343f8beb833784e
Arora Nirav <aroranirav2@outlook.com>
2022-11-08 15:57:44 -0600

    add question 1544

---
+++ Q1544MakeTheStringGreat/Q1544MakeTheStringGreat/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace Q1544MakeTheStringGreat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MakeGood("leEeetcode"));
+        }
+
+        public static string MakeGood(string s)
+        {
+            for (int i = 0; i < s.Length - 1; i++)
+            {
+                if (Math.Abs(s[i] - s[i + 1]) == 32)
+                    return MakeGood(s.Substring(0, i) + s.Substring(i + 2));
+            }
+            return s;
+        }
+    }
+}
